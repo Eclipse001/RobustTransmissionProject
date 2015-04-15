e8e4f0f60985a9a0b05a9b9cbde99388d1a7a319
Yggdrasil-0 <xuping@ualberta.ca>
2015-04-14 22:44:57 -0600

    Xiaoran.like(Ji)

--- src/singleRecoveryMethods/TempReplacement.java
+++ src/singleRecoveryMethods/TempReplacement.java
@@ -6,21 +6,33 @@ import java.util.ArrayList;
 
 import assistClass.InputData;
 
-
+/***
+ * A recovery method class which uses the forward temp replacement to the BVH frame data which experienced frame loss during the transmission simulation.
+ * @author Xuping Fang
+ */
 public class TempReplacement {
 	
 	private String recoveredFilePath;
 	
 	private InputData inputData;
 	
+	/**
+	 * Constructor of this recovery method class.
+	 * @param inputData : An InputData object which contains all required information of the BVH frame data which experienced frame loss during the transmission simulation.
+	 * @param recoveredFilePath : The location of the recovered BVH file will be created. 
+	 */
 	public TempReplacement(InputData inputData,String recoveredFilePath){
 		
 		this.inputData=inputData;
 		this.recoveredFilePath=recoveredFilePath;
 	}
 	
+	/**
+	 * The main recovery method, replace each null frame with the previous valid frame if exist, otherwise replace it the frame with zeros.
+	 */
 	public void runRecovery(){
 		
+		// Fetch the frame data from the InputData object.
 		ArrayList<ArrayList<Double>> frames=this.inputData.getBadDataCopy();
 		
 		ArrayList<Double> prevValidFrame=null;
@@ -31,6 +43,7 @@ public class TempReplacement {
 			
 			currentFrame=frames.get(index);
 			
+			// If the current frame is null, replace it with the previous valid frame if exist, otherwise replace it the frame with zeros.
 			if(currentFrame==null){
 				
 				if(prevValidFrame==null){
@@ -47,9 +60,14 @@ public class TempReplacement {
 			}
 		}
 		
+		// Write the recovery file.
 		this.writeFile(frames);
 	}
 	
+	/**
+	 * A private method which writes the new BVH file after recovery in to the preset location.
+	 * @param frames : an ArrayList<ArrayList<Double>> object which contains the frame data after recovery.
+	 */
 	private void writeFile(ArrayList<ArrayList<Double>> frames){
 		
 		PrintWriter printWriter;
@@ -57,12 +75,15 @@ public class TempReplacement {
 		try {
 			printWriter = new PrintWriter(this.recoveredFilePath,"UTF-8");
 			
+			// Write the header of the file.
 			for(String line : this.inputData.getBvhHeader()){
 				printWriter.println(line);
 			}
 			
+			// Write each frame of the frame data after recovery.
 			for(int index=0;index<this.inputData.getNumOfFrames();index++){
 				
+				// Construct a string based on the frame data in the ArrayList.
 				String line="";
 				
 				ArrayList<Double> currentFrame=frames.get(index);
@@ -93,6 +114,11 @@ public class TempReplacement {
 		}
 	}
 	
+	/**
+	 * A private method which takes an ArrayList<Double> type as the input parameter, and returns an independent copy of it.
+	 * @param src : An ArrayList<Double> as the list which will be copied.
+	 * @return : An independent copy of the input parameter.
+	 */
 	private ArrayList<Double> makeCopy(ArrayList<Double> src){
 		
 		ArrayList<Double> cpy=new ArrayList<Double>();
@@ -104,6 +130,10 @@ public class TempReplacement {
 		return cpy;
 	}
 	
+	/**
+	 * A private method which returns an ArrayList filled with zero which represents the frame with just zero based on the number of joint data of each frame fetched from the InputData.
+	 * @return : An ArrayList filled with zero.
+	 */
 	private ArrayList<Double> getLineWithAllZeros(){
 		
 		ArrayList<Double> zeros= new ArrayList<Double>();
--- src/singleRecoveryMethods/TempReplacementBackward.java
+++ src/singleRecoveryMethods/TempReplacementBackward.java
@@ -6,21 +6,33 @@ import java.util.ArrayList;
 
 import assistClass.InputData;
 
-
+/***
+ * A recovery method class which uses the backward temp replacement to the BVH frame data which experienced frame loss during the transmission simulation.
+ * @author Xuping Fang
+ */
 public class TempReplacementBackward {
 	
 	private String recoveredFilePath;
 	
 	private InputData inputData;
 	
+	/**
+	 * Constructor of this recovery method class.
+	 * @param inputData : An InputData object which contains all required information of the BVH frame data which experienced frame loss during the transmission simulation.
+	 * @param recoveredFilePath : The location of the recovered BVH file will be created. 
+	 */
 	public TempReplacementBackward(InputData inputData,String recoveredFilePath){
 		
 		this.inputData=inputData;
 		this.recoveredFilePath=recoveredFilePath;
 	}
 	
+	/**
+	 * The main recovery method, replace each null frame with the next valid frame if exist, otherwise replace it the frame with zeros.
+	 */
 	public void runRecovery(){
 		
+		// Fetch the frame data from the InputData object.
 		ArrayList<ArrayList<Double>> frames=this.inputData.getBadDataCopy();
 		
 		ArrayList<Double> prevValidFrame=null;
@@ -31,6 +43,7 @@ public class TempReplacementBackward {
 			
 			currentFrame=frames.get(index);
 			
+			// If the current frame is null, replace it with the previous valid frame if exist, otherwise replace it the frame with zeros.
 			if(currentFrame==null){
 				
 				if(prevValidFrame==null){
@@ -47,9 +60,14 @@ public class TempReplacementBackward {
 			}
 		}
 		
+		// Write the recovery file.
 		this.writeFile(frames);
 	}
 	
+	/**
+	 * A private method which writes the new BVH file after recovery in to the preset location.
+	 * @param frames : an ArrayList<ArrayList<Double>> object which contains the frame data after recovery.
+	 */
 	private void writeFile(ArrayList<ArrayList<Double>> frames){
 		
 		PrintWriter printWriter;
@@ -57,12 +75,15 @@ public class TempReplacementBackward {
 		try {
 			printWriter = new PrintWriter(this.recoveredFilePath,"UTF-8");
 			
+			// Write the header of the file.
 			for(String line : this.inputData.getBvhHeader()){
 				printWriter.println(line);
 			}
 			
+			// Write each frame of the frame data after recovery.
 			for(int index=0;index<this.inputData.getNumOfFrames();index++){
 				
+				// Construct a string based on the frame data in the ArrayList.
 				String line="";
 				
 				ArrayList<Double> currentFrame=frames.get(index);
